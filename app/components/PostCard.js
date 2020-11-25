e3c0d2961adb733e194b72970b7cc473d8fd0518
Elfego Melgar <melgarelfego@gmail.com>
2020-11-24 23:06:05 -0600

    Codigo agregado en PostCard

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,4 +1,20 @@
 export function PostCard(props){
-    return 
+    let {date,id,slug,title,_embedded} = props;
+    let dateFormat = new Date(date).toLocaleString(),
+        urlPoster = _embedded["wp:featuredmedia"]
+        ?_embedded["wp:featuredmedia"][0].source_url
+        : "app/assets/favicon.png";
+    
+
+        return `
+        <article class="post-card">
+            <img src="${urlPoster}" alt="${title.rendered}">
+            <h2>${title.rendered}</h2>
+            <p>
+                <time datatime="${date}">${dateFormat}</time>
+                <a href="#/${slug}" data-id="${id}">Ver publicacion</a>
+            </p>
+        </article>
+    `;
     
 }
